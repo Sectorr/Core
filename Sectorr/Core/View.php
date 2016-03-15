6e63d746d16a905b3193d00630523ff002814363
Mirko Kroese <mirkokroese@gmail.com>
2016-03-15 15:28:57 +0100

    Fixed blade

--- Sectorr/Core/View.php
+++ Sectorr/Core/View.php
@@ -13,7 +13,7 @@ class View
     {
         $blade = new BladeInstance(PATH . "/app/Views", sys_get_temp_dir());
 
-        $location = PATH . '/app/Views/' . $view . '.php';
+        $location = PATH . '/app/Views/' . $view . '.blade.php';
 
         if (! file_exists($location)) {
             throw new ViewNotFoundException($view);
