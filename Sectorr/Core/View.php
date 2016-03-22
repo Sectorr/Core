9b1f911679a8eef62873df63a399deee4a53eae1
Janyk Steenbeek <info@janyksteenbeek.nl>
2016-03-22 11:57:16 +0100

    Added support for dots in view path

--- Sectorr/Core/View.php
+++ Sectorr/Core/View.php
@@ -13,6 +13,7 @@ class View
     {
         $blade = new BladeInstance(PATH . "/app/Views", sys_get_temp_dir());
 
+        $view = str_replace('.', '/', $view);
         $location = PATH . '/app/Views/' . $view . '.blade.php';
 
         if (! file_exists($location)) {
