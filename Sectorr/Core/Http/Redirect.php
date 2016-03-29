f5bc7018eb9901908cc0717afe5c87246f30e18d
Janyk Steenbeek <info@janyksteenbeek.nl>
2016-03-29 15:12:17 +0200

    Refactored Redirect and added Back function.

--- Sectorr/Core/Http/Redirect.php
+++ Sectorr/Core/Http/Redirect.php
@@ -6,25 +6,57 @@ class Redirect
 {
 
     /**
+     * Sets redirect header.
+     *
+     * @param $url
+     * @return mixed
+     */
+    protected static function redirect($url)
+    {
+        return header("Location: {$url}");
+    }
+
+    /**
      * Redirects to the given url.
      *
      * @param $url
+     * @return mixed
      */
     public static function url($url)
     {
         if (substr($url, 0, 1) == '/') {
             $url = substr_replace($url, '', 0, 1);
         }
-        header("Location: /{$url}");
+
+        return self::redirect("/{$url}");
     }
 
     /**
      * Redirects to the given route.
      *
      * @param $route
+     * @param array $properties
+     * @return mixed
+     * @throws \Sectorr\Core\Exceptions\RouteNotFoundException
      */
     public static function route($route, $properties = [])
     {
-        self::url(Route::findRoute($route, $properties));
+        return self::url(Route::findRoute($route, $properties));
+    }
+
+    /**
+     * Redirect to HTTP referer.
+     *
+     * @return mixed
+     */
+    public static function back()
+    {
+        $referer = $_SERVER['HTTP_REFERER'];
+
+        if (! empty($referer)) {
+            return self::redirect($referer);
+        }
+
+        return self::redirect('/');
     }
 }
